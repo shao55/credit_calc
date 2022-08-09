8e36f224f6b1a676e0edd218d4242bb419c31338
Nurgissa <nurgissa.22@gmail.com>
2022-08-09 16:32:47 +0600

    Day 1

---
+++ js/formatters.js
@@ -0,0 +1,6 @@
+export const percent_formatter = new Intl.NumberFormat("ru-RU", 
+    {
+        style: "percent", 
+        maximumFractionDigits: 3
+    }
+);
---
+++ js/program_rates.js
@@ -0,0 +1,32 @@
+import { percent_formatter } from "./formatters.js";
+// Процентные ставки
+const program_base = 0.12;
+const program_it = 0.047;
+const program_gov = 0.067;
+const program_zero = 0.108;
+
+// Отображение процентной ставки на страницу
+document.querySelector('#base-value').value = program_base;
+document.querySelector('#it-value').value = program_it;
+document.querySelector('#gov-value').value = program_gov;
+document.querySelector('#zero-value').value = program_zero;
+
+// Указываем процентную ставку в Label
+document.querySelector("#base-text").innerText = percent_formatter.format(program_base);
+document.querySelector("#it-text").innerText = percent_formatter.format(program_it);
+document.querySelector("#gov-text").innerText = percent_formatter.format(program_gov);
+document.querySelector("#zero-text").innerText = percent_formatter.format(program_zero);
+
+// Отображаем выбранную процентную ставку в правом блоке
+const program_inputs = document.querySelectorAll("input[name='program']");
+const total_percent = document.querySelector("#total-percent");
+program_inputs.forEach((input) => {
+    // Отображение процентной ставки на старте
+    if (input.checked) {
+        total_percent.innerText = percent_formatter.format(input.value);
+    }
+    // Отображение процентной ставки при переключении
+    input.addEventListener("click", function () {
+        total_percent.innerText = percent_formatter.format(this.value);
+    })
+});
