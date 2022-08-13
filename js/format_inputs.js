7ee10b1110a9bc44cd6eef69b8e753fdae0fec62
Nurgissa <nurgissa.22@gmail.com>
2022-08-13 23:13:50 +0600

    Day 3

--- js/format_inputs.js
+++ js/format_inputs.js
@@ -1,7 +1,10 @@
+import {price_formatter} from "./formatters.js"
 // Находим инпуты на страницы
 const input_cost = document.querySelector("#input-cost");
 const input_down_payment = document.querySelector("#input-downpayment");
 const input_term = document.querySelector("#input-term");
+const form = document.querySelector("#form");
+const total_cost = document.querySelector("#total-cost");
 
 // Опции форматирования от cleavejs
 const cleave_price_settings = {
@@ -14,4 +17,17 @@ const cleave_price_settings = {
 const cleave_cost = new Cleave(input_cost, cleave_price_settings);
 const cleave_downpayment = new Cleave(input_down_payment, cleave_price_settings);
 
+// Вызов функции расчета суммы кредита в начале
+calc_mortgage();
+
 // Отображение и расчет суммы кредита
+form.addEventListener("input", function () {
+    // Вызов функции расчета суммы кредита после изменений
+    calc_mortgage()
+});
+
+// Функция расчета суммы кредита (общая сумма минус первоначальный взнос)
+function calc_mortgage() {
+    const total_amount = +cleave_cost.getRawValue() - +cleave_downpayment.getRawValue();
+    total_cost.innerText = price_formatter.format(total_amount);
+};
--- js/formatters.js
+++ js/formatters.js
@@ -1,6 +1,16 @@
+// Форматирует в проценты 4,752%
 export const percent_formatter = new Intl.NumberFormat("ru-RU", 
     {
         style: "percent", 
         maximumFractionDigits: 3
+    }
+);
+
+// Форматирует число в 7 000 000 Р
+export const price_formatter = new Intl.NumberFormat("ru-RU",
+    {
+        style: "currency", 
+        currency: "RUB", 
+        maximumFractionDigits: 2 
     }
 );
